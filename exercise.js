a8bb0b8ce887d844b48e1423a135e9802623050a
Ross MacDonald <rmac2289@gmail.com>
2020-02-19 20:16:46 -0800

    nearly finalized styling and added nutritional fact functioning

--- exercise.js
+++ exercise.js
@@ -1,7 +1,9 @@
+/* spoonacular API key and base URL used in both fetches */ 
+
 const apiKey = 'e665e607a65e442b806a7f9495411f15'
 const baseURL = 'https://api.spoonacular.com/'
 
-/* PARAMATER FORMATTING FUNCTION */ 
+/* properly formats paramters in fetch request*/ 
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -9,7 +11,7 @@ function formatQueryParams(params) {
     return queryItems.join('&');
   }
 
-  /* INGREDIENT DISPLAY */ 
+  /* handles what should be displayed when the user clicks the 'find substitute' button */ 
 
 function displayIngredient(responseJson){
     console.log(responseJson);
@@ -28,16 +30,17 @@ function displayIngredient(responseJson){
     };
 
     }else {
+        $('.subList').empty();
         $('.mainForm').addClass('formWidth');
     $('.formList').addClass('regulateWidth');
         $('.listContainer').removeClass('hidden');
         $('.subList').append(`<h3 class="notFound formWidth">Sorry, couldn't find any substitutes for that ingredient!</h3>`)
     }
 }
-/* MEAL PLAN DISPLAY */ 
+/* handles what should be displayed when the user clicks the 'lets get cookin'' button */ 
 
 function displayMealPlan(responseJson) {
-    console.log(responseJson.week);
+    console.log(responseJson);
     let data = responseJson.week;
     let days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
@@ -50,10 +53,15 @@ function displayMealPlan(responseJson) {
             <li><h4><span class="meal">lunch:</span><a class="mealLink" href="${data[days[i]].meals[1].link}" target="_blank"> ${data[days[i]].meals[1].cleanTitle}</a></h4></li>
             <li><h4><span class="meal">dinner:</span><a class="mealLink" href="${data[days[i]].meals[2].link}" target="_blank"> ${data[days[i]].meals[2].cleanTitle}</a></h4></li>
         </ul>
-            `);
+        <ul class="nutrition">
+            <li><span id="calorieCount"> calories:</span> ${data[days[i]].nutrients.calories} <span class="divider">||</span> <span id="proteinCount">protein:</span> ${data[days[i]].nutrients.protein} 
+            <span class="divider">||</span> <span id="fatCount">fat:</span> ${data[days[i]].nutrients.fat} <span class="divider">||</span> <span id="carbCount">carbs:</span> ${data[days[i]].nutrients.carbohydrates}</li>
+        </ul>
+                   `);
         }}
 
-/* MEAL FETCH */ 
+/* fetches the spoonacular meal planning API - the paramEditor function filters which paramater
+set to use based on what input is provided by the user  */ 
 
 
 function getMeals(diet,calories,exclude){
@@ -121,7 +129,9 @@ function getMeals(diet,calories,exclude){
             })
         }
 
-    /* ingredient API fetch */
+    /* fetches the spoonacular ingredient substitution API - if the JSON response 
+    returns as it should, the JSON data is passed to the displayIngredient function,
+    if not, an error is displayed */
 
         function getIngredient(ingredient) {
             const params = {
@@ -147,7 +157,8 @@ function getMeals(diet,calories,exclude){
         
         }
 
-/* ON SUBMITTING FORM FUNCTION */
+/* when user clicks the 'find substitute' button, initiates variable 'ingredient' 
+containing the value that the user entered, and passes that value to getIngredient function */
 
 function ingredientForm(){
     $('#ingredientButton').click(function(){
@@ -156,6 +167,17 @@ function ingredientForm(){
         getIngredient(ingredient);
     })
 }
+
+/* automatically scrolls down 550px to meal list after clicking 'let's get cookin'' */
+
+function handleScroll(){
+$(".mainButton").click(function(event){
+    $('html, body').animate({scrollTop: '+=550px'}, 1200);
+});
+}
+
+/* handles submission of the main form, including the diet, calorie, and exclude 
+values entered by the user, and passes those values to the getMeals function */
 
 function watchForm() {
     $('.mainButton').click(function(event){
@@ -172,3 +194,4 @@ function watchForm() {
 
 $(ingredientForm);
 $(watchForm);
+$(handleScroll);
